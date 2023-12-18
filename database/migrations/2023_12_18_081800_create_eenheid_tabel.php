43352521c034b8efa6b14b0673a79844230ae650
Sing <singfong91@gmail.com>
2023-12-18 13:04:35 +0100

    migrations gefixed

--- database/migrations/2023_12_18_081800_create_eenheid_tabel.php
+++ database/migrations/2023_12_18_081800_create_eenheid_tabel.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('eenheid', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name')->nullable(false);
         });
     }
 
