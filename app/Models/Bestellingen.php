dd6ac7da2c0c8d460ada434002386ed08bca2e28
Sing <singfong91@gmail.com>
2023-12-19 19:40:42 +0100

    Klanten.php model aangemaakt ( was vergeten )

--- app/Models/Bestellingen.php
+++ app/Models/Bestellingen.php
@@ -24,7 +24,7 @@ class bestellingen extends Model
 
     public function customer()
     {
-        return $this->belongsTo(Customer::class);
+        return $this->belongsTo(Klanten::class);
     }
 
     public function price()
--- app/Models/medewerkers.php
+++ app/Models/medewerkers.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class medewerkers extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function orders()
+    {
+        return $this->hasMany(bestellingen::class);
+    }
 }
