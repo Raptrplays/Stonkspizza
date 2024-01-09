7296630b83073cd54de7a83f8f9482869056ac06
giulia <murariu.giulia@gmail.com>
2024-01-09 18:35:53 +0100

    styles aangepast menu en mandje

--- resources/views/mandje.blade.php
+++ resources/views/mandje.blade.php
@@ -19,12 +19,12 @@
 
                 @if (Route::has('login'))
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-orange-400 hover:text-orange-300 ml-1">Account</a>
+                        <a href="{{ url('/dashboard') }}" class="font-semibold bg-orange-400 text-white rounded-full px-4 py-2 hover:text-orange-300 ml-1">Account</a>
                     @else
                         <a href="{{ route('login') }}" class="font-semibold text-orange-400 hover:text-orange-300 ml-1">Inloggen</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="font-semibold text-orange-400 hover:text-orange-300 ml-1">Registreren</a>
+                            <a href="{{ route('register') }}" class="font-semibold bg-orange-400 text-white rounded-full px-4 py-2 hover:text-orange-300 ml-1">Registreren</a>
                         @endif
                     @endauth
                 </div>
@@ -32,6 +32,60 @@
             </nav>
         </div>
     </header>
+
+<section class="flex">
+    <div class="w-2/3 bg-white m-10 p-10 rounded-md shadow-md">
+        <h1 class="text-2xl font-bold mb-6">Jou Bestelling</h1>
+        @foreach($order as $item)
+        <div>
+            <h1 class="font-bold">{{ $item->name }}</h1>
+            <div class="mt-4">
+                <label class="block text-black mb-2" for="grootte">Kies grootte van de pizza</label>
+            <select id="grootte" name="grootte">
+                @foreach ($groottes as $grootte)
+                  <option value="{{ $grootte->id }}">{{ $grootte->name }}</option>
+                @endforeach
+            </select>
+            <label class="block text-black mb-2 mt-4" for="ingredient">Extra ingredient</label>
+            <select id="ingredient" name="ingredient">
+                @foreach ($ingredienten as $ingredient)
+                  <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
+                @endforeach
+            </select>
+
+            <label class="block text-black mb-2 mt-4" for="ingredient2">Verwijder ingredient</label>
+            <select id="ingredient2" name="ingredient2">
+                @foreach ($ingredienten as $ingredient)
+                  <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
+                @endforeach
+            </select>
+            </div>
+            <form action="{{ url('mandje/' . $item->id) }}" method="post">
+                @csrf
+                @method('delete')
+                <button type="submit" class="bg-red-600 hover:bg-red-500 text-yellow-50 py-2 px-4 mt-5 rounded-full">Verwijder van bestelling</button>
+            </form>
+        </div>
+        @endforeach
+    </div>
+
+    <div class="w-1/3 bg-slate-100 m-10 p-10 rounded-md shadow-md">
+        <form action="/bereken" method="get">
+            @csrf
+            <input type="hidden" name="ingredient" id="ingredient1" value="{{ $ingredient->id }}">
+            <input type="hidden" name="ingredient2" id="ingredient2" value="{{ $ingredient->id }}">
+            <input type="hidden" name="grootte" id="grootte" value="{{ $grootte->id }}">
+            <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-1 px-2 mt-5 rounded">Bereken prijs</button>
+        </form>
+
+        {{--  @if (isset($totaalprijs))
+        <h1>Totaalprijs: {{ $Totaalprijs }}</h1>
+        @endif  --}}
+        <h1>Totaalprijs</h1>
+
+        <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-1 px-2 mt-5 rounded">Bestellen</button>
+    </div>
+</section>
 
     <div class="flex">
         @foreach($order as $item)
@@ -69,9 +123,7 @@
     </div>
 
     <div class="flex-1 bg-yellow-50 p-4 m-10 shadow-md shadow-black rounded-sm">
-        @if (isset($totaalprijs))
-        <h1>Totaalprijs: {{ $Totaalprijs }}</h1>
-        @endif
+        
 
         <form action="/bereken" method="get">
             @csrf
@@ -81,8 +133,12 @@
             <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-1 px-2 mt-5 rounded">Bereken prijs</button>
         </form>
 
+        {{--  @if (isset($totaalprijs))
+        <h1>Totaalprijs: {{ $Totaalprijs }}</h1>
+        @endif  --}}
+        <h1>Totaalprijs</h1>
+
         <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-1 px-2 mt-5 rounded">Bestellen</button>
     </div>
-
 </body>
 </html>
--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Ingredienten beheren</title>
+    <title>Menu</title>
 </head>
 <body class="bg-yellow-50">
     <header class="bg-green-800 text-white p-4">
@@ -22,12 +22,12 @@
 
                 @if (Route::has('login'))
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-orange-400 hover:text-orange-300 ml-1">Account</a>
+                        <a href="{{ url('/dashboard') }}" class="font-semibold bg-orange-400 text-white rounded-full px-4 py-2 hover:text-orange-300 ml-1">Account</a>
                     @else
                         <a href="{{ route('login') }}" class="font-semibold text-orange-400 hover:text-orange-300 ml-1">Inloggen</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="font-semibold text-orange-400 hover:text-orange-300 ml-1">Registreren</a>
+                            <a href="{{ route('register') }}" class="font-semibold bg-orange-400 text-white rounded-full px-4 py-2 hover:text-orange-300 ml-1">Registreren</a>
                         @endif
                     @endauth
                 </div>
@@ -36,25 +36,40 @@
         </div>
     </header>
 
-    <div class="flex">
+    <section class="relative">
+        <img src="https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Delicious Pizza" class="w-full h-64 object-cover">
+        <div class="absolute inset-0 bg-black opacity-30"></div>
+        <div class="absolute inset-0 flex items-center justify-center">
+            <h2 class="text-4xl font-bold text-white">Ontdek de smaak van perfectie</h2>
+        </div>
+    </section>
+
+    <section class="grid grid-cols-2 gap-8 m-10">
         @foreach($pizzas as $pizza)
-        <div class="flex-1 bg-yellow-50 p-4 m-10 shadow-md shadow-black rounded-sm">
-            <h1>{{ $pizza->name }}</h1>
-            <br>
-            <p>{{ $pizza->beschrijving}}</p>
+        <div class="bg-white p-6 rounded-lg shadow-md">
+                <img src="https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGl6emElMjB0b3AlMjBwb3Z8ZW58MHwwfDB8fHwy" alt="foto" class="w-full h-48 object-cover mb-4 rounded-md">
+                <h2 class="text-xl font-bold mb-2">{{ $pizza->name }}</h2>
+                <p class="text-gray-700">{{ $pizza->beschrijving }}</p>
+
             @guest
             <button type="button" disabled class="bg-orange-400 text-white py-1 px-2 mt-2 rounded">Log in om toe te voegen aan je bestelling</button>
             @endguest
+
             @auth
             <form id="orderForm" action="/bestel" method="get">
                 @csrf
                 <input type="hidden" name="pizza_id" value="{{ $pizza->id }}">
-                <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-1 px-2 mt-2 rounded">Voeg toe aan bestelling</button>
+                <button class="mt-4 bg-orange-400 hover:bg-orange-300 text-white px-4 py-2 rounded-full">Toevoegen aan bestelling</button>
             </form>
             @endauth
+
         </div>
         @endforeach
-    </div>
+    </section>
+
+    <footer class="bg-green-800 text-white p-4 text-center">
+        <p>&copy; 2024 StonksPizza</p>
+    </footer>
 
 </body>
 </html>
