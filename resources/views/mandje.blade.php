888dbbed891b6b49ae76f2413a696405dae22f5f
giulia <murariu.giulia@gmail.com>
2024-01-11 16:34:50 +0100

    comments toegevoegd

--- resources/views/mandje.blade.php
+++ resources/views/mandje.blade.php
@@ -33,59 +33,69 @@
         </div>
     </header>
 
-<section class="flex flex-wrap lg:flex-nowrap">
-    <div class="lg:w-2/3 md:w-full w-full bg-white m-10 p-10 rounded-md shadow-md">
-        <h1 class="text-2xl font-bold mb-6">Jou Bestelling</h1>
-        @foreach($order as $item)
-        <div>
-            <h1 class="font-bold">{{ $item->name }}</h1>
-            <div class="mt-4">
-                <label class="block text-black mb-2" for="grootte">Kies grootte van de pizza</label>
-            <select id="grootte" name="grootte">
-                @foreach ($groottes as $grootte)
-                  <option value="{{ $grootte->id }}">{{ $grootte->name }}</option>
-                @endforeach
-            </select>
-            <label class="block text-black mb-2 mt-4" for="ingredient">Extra ingredient</label>
-            <select id="ingredient" name="ingredient">
-                @foreach ($ingredienten as $ingredient)
-                  <option id="ingredient" value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
-                @endforeach
-            </select>
-
-            <label class="block text-black mb-2 mt-4" for="ingredient2">Verwijder ingredient</label>
-            <select id="ingredient2" name="ingredient2">
-                @foreach ($ingredienten as $ingredient)
-                  <option id="ingredient2" value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
-                @endforeach
-            </select>
+    <section class="flex flex-wrap lg:flex-nowrap">
+        <div class="lg:w-2/3 md:w-full w-full bg-white m-10 p-10 rounded-md shadow-md">
+            <h1 class="text-2xl font-bold mb-6">Jou Bestelling</h1>
+            @foreach($order as $item)
+            <div>
+                <h1 class="font-bold">{{ $item->name }}</h1>
+                <div class="mt-4">
+                    <label class="block text-black mb-2" for="grootte">Kies grootte van de pizza</label>
+                <select id="grootte" name="grootte">
+                    @foreach ($groottes as $grootte)
+                      <option value="{{ $grootte->id }}">{{ $grootte->name }}</option>
+                    @endforeach
+                </select>
+                <label class="block text-black mb-2 mt-4" for="ingredient">Extra ingredient</label>
+                <select id="ingredient" name="ingredient">
+                    @foreach ($ingredienten as $ingredient)
+                      <option id="ingredient" value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
+                    @endforeach
+                </select>
+    
+                <label class="block text-black mb-2 mt-4" for="ingredient2">Verwijder ingredient</label>
+                <select id="ingredient2" name="ingredient2">
+                    @foreach ($ingredienten as $ingredient)
+                      <option id="ingredient2" value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
+                    @endforeach
+                </select>
+                </div>
+                <form action="{{ url('mandje/' . $item->id) }}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" class="bg-red-600 hover:bg-red-500 text-yellow-50 py-2 px-4 mt-5 rounded-full">Verwijder van bestelling</button>
+                </form>
             </div>
-            <form action="{{ url('mandje/' . $item->id) }}" method="post">
-                @csrf
-                @method('delete')
-                <button type="submit" class="bg-red-600 hover:bg-red-500 text-yellow-50 py-2 px-4 mt-5 rounded-full">Verwijder van bestelling</button>
-            </form>
+            @endforeach
         </div>
-        @endforeach
+    
+        <div class="w-full lg:w-1/3 md:w-full bg-slate-100 m-10 p-10 rounded-md shadow-md">
+            <h1 class="text-2xl font-bold mb-6">Totaal</h1>
+            {{--  @if (isset($totaalprijs))
+            <h1>Totaalprijs: {{ $Totaalprijs }}</h1>
+            @endif  --}}
+    
+            <form action="/bereken" method="get">
+                @csrf
+                <input type="hidden" name="ingredient" id="ingredient1" value="{{ $ingredient->id }}">
+                <input type="hidden" name="ingredient2" id="ingredient2" value="{{ $ingredient->id }}">
+                <input type="hidden" name="grootte" id="grootte" value="{{ $grootte->id }}">
+                <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-1 px-2 mt-5 rounded">Bereken prijs</button>
+            </form>
+    
+            <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-1 px-2 mt-5 rounded">Bestellen</button>
+        </div>
+    </section>
+    {{--
+    <div class="flex items-center justify-center m-8">
+        <a href="{{ url('/menu') }}" class="font-semibold bg-orange-400 text-white rounded-full px-4 py-2 hover:text-orange-300">Menu</a>
     </div>
-
-    <div class="w-full lg:w-1/3 md:w-full bg-slate-100 m-10 p-10 rounded-md shadow-md">
-        <h1 class="text-2xl font-bold mb-6">Totaal</h1>
-        {{--  @if (isset($totaalprijs))
-        <h1>Totaalprijs: {{ $Totaalprijs }}</h1>
-        @endif  --}}
-
-        <form action="/bereken" method="get">
-            @csrf
-            <input type="hidden" name="ingredient" id="ingredient1" value="{{ $ingredient->id }}">
-            <input type="hidden" name="ingredient2" id="ingredient2" value="{{ $ingredient->id }}">
-            <input type="hidden" name="grootte" id="grootte" value="{{ $grootte->id }}">
-            <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-1 px-2 mt-5 rounded">Bereken prijs</button>
-        </form>
-
-        <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-1 px-2 mt-5 rounded">Bestellen</button>
+    <div class="flex items-center justify-center mt-10">
+        <h2 class="text-4xl font-bold ">Kies eerst een pizza</h2>
     </div>
-</section>
+    --}}
+
+
 
 </body>
 </html>
