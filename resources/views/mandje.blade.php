40198f29038c6449c813664b852859ab88bd1b3f
Sing <singfong91@gmail.com>
2024-01-15 12:21:20 +0100

    delete werkt

--- resources/views/mandje.blade.php
+++ resources/views/mandje.blade.php
@@ -41,7 +41,7 @@
         <h2 class="text-4xl font-bold ">Kies eerst een pizza</h2>
     </div>
     --}}
-   
+
 
     <section class="flex flex-wrap lg:flex-nowrap">
         <div class="lg:w-2/3 md:w-full w-full bg-white m-10 p-10 rounded-md shadow-md">
@@ -57,12 +57,12 @@
                     @endforeach
                 </select>
                 <label class="block text-black mb-2 mt-4" for="ingredient">Extra ingredient</label>
-                <select id="ingredient" name="ingredient">
+                <select id="ingredient" name="ingredient" onchange="document.getElementById('ingredient').value = this.value;">
                     @foreach ($ingredienten as $ingredient)
-                      <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
+                        <option value="{{ $ingredient->id }}">{{ $ingredient->name }}</option>
                     @endforeach
                 </select>
-    
+
                 <label class="block text-black mb-2 mt-4" for="ingredient2">Verwijder ingredient</label>
                 <select id="ingredient2" name="ingredient2" onchange="document.getElementById('ingredienttwee').value = document.getElementById('ingredient2').value">
                     @foreach ($ingredienten as $ingredient)
@@ -78,21 +78,26 @@
             </div>
             @endforeach
         </div>
-    
+
         <div class="w-full lg:w-1/3 md:w-full bg-slate-100 m-10 p-10 rounded-md shadow-md">
             <h1 class="text-2xl font-bold mb-6">Totaalprijs</h1>
             @if (isset($totaalprijs))
-            <h1>€{{ number_format($totaalprijs, 2, ',', '.') }}</h1>
+                <h1>€{{ number_format($totaalprijs, 2, ',', '.') }}</h1>
             @endif
 
-            <form action="/bereken" method="post">
-                @csrf
-                <input type="hidden" name="ingredient" id="ingredient1" value="{{ $ingredient->id }}">
-                <input type="hidden" name="ingredient2" id="ingredienttwee" value="{{ $ingredient->id }}">
-                <input type="hidden" name="grootte" id="grootte" value="{{ $grootte->id }}">
-                <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-2 px-4 mt-5 mb-5 rounded-full">Bereken prijs</button>
-            </form>
-    
+            @if (!empty($ingredient) && !empty($grootte))
+                <form action="mandje" method="post">
+                    @csrf
+                    <input type="hidden" name="ingredient" id="ingredient1" value="{{ $ingredient->id }}">
+                    <input type="hidden" name="ingredient2" id="ingredienttwee" value="{{ $ingredient->id }}">
+                    <input type="hidden" name="grootte" id="grootte" value="{{ $grootte->id }}">
+                    <button type="submit" class="bg-orange-400 hover:bg-orange-300 text-white py-2 px-4 mt-5 mb-5 rounded-full">Bereken prijs</button>
+                </form>
+            @else
+                <p>U heeft nog geen pizzas geselecteerd.</p>
+            @endif
+            <br>
+
             <a href="{{ url('/status') }}" class="bg-orange-400 text-white rounded-full px-4 py-2 hover:bg-orange-300">Bestellen</a>
         </div>
     </section>
