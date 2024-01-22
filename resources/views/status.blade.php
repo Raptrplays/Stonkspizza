87e983652c9154044fabbfb02964f8004cfe1d1d
giulia <murariu.giulia@gmail.com>
2024-01-22 12:45:34 +0100

    status icons toegevoegd

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/732b8d41cd.js" crossorigin="anonymous"></script>   
     @vite('resources/css/app.css')
     <title>Status van de bestelling</title>
 </head>
@@ -39,7 +40,8 @@
     @if ($statusid > 5)
 
     <div class="flex items-center justify-center mt-10">
-        <div class="bg-red-600 p-8 rounded-md shadow-md">
+        <div class="bg-red-600 p-8 rounded-md shadow-md flex flex-col items-center justify-center">
+            <i class="fas fa-exclamation-triangle text-4xl"></i>
             <h1 class="text-2xl font-bold text-white">Je bestelling is geannuleerd</h1>
         </div>
     </div>
@@ -48,31 +50,42 @@
     <section class="flex flex-wrap lg:flex-nowrap mt-10">
         <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 m-8 p-10 rounded-md shadow-md
         @if($statusid == 1) bg-orange-400 text-white @endif
-        ">
+        flex flex-col items-center justify-center">
+
+            <i class="fa-solid fa-shop text-4xl"></i>
             <h1 class="text-2xl font-bold text-center">Besteld</h1>
         </div>
 
         <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 m-8 p-10 rounded-md shadow-md
         @if($statusid == 2) bg-orange-400 text-white @endif
-        ">
+        flex flex-col items-center justify-center">
+    
+            <i class="fa-solid fa-utensils text-4xl"></i>
             <h1 class="text-2xl font-bold text-center">Wordt bereid</h1>
         </div>
+    
 
         <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 m-8 p-10 rounded-md shadow-md
         @if($statusid == 3) bg-orange-400 text-white @endif
-        ">
+        flex flex-col items-center justify-center">
+
+            <i class="fas fa-thermometer-three-quarters text-4xl"></i>
             <h1 class="text-2xl font-bold text-center">In de oven</h1>
         </div>
 
         <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 m-8 p-10 rounded-md shadow-md
         @if($statusid == 4) bg-orange-400 text-white @endif
-        ">
+        flex flex-col items-center justify-center">
+
+            <i class="fa-solid fa-person-biking text-4xl"></i>
             <h1 class="text-2xl font-bold text-center">Is onderweg</h1>
         </div>
         
         <div class="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 m-8 p-10 rounded-md shadow-md
         @if($statusid == 5) bg-orange-400 text-white @endif
-        ">
+        flex flex-col items-center justify-center">
+            
+            <i class="fa-solid fa-map-pin text-4xl"></i>
             <h1 class="text-2xl font-bold text-center">Bezorgd</h1>
         </div>
     </section>
