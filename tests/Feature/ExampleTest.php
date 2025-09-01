306e6a0a59d17fa1c8571a790d17fa42dd6e6f35
giulia <murariu.giulia@gmail.com>
2025-09-01 13:44:39 +0200

    edited feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,7 +12,9 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $user = \App\Models\User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/');
 
         $response->assertStatus(200);
     }
